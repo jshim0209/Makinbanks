e7a322f5cbf5e3facb122347d958e2a0d2d9cdaf
jshim0209 <100379451+jshim0209@users.noreply.github.com>
2024-05-11 00:53:39 -0400

    authentication implemented using Clerk

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1>Test Page</h1>
+    </div>
+  );
+};
+
+export default TestPage;
